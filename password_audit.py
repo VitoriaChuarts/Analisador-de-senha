21c017ef6f8f6455982a1c7080c823b5f505437f
Vitória Chuarts <vitoriachuarts@icloud.com>
2026-08-18 15:58:50 +0000

    adiciona caracteres

--- password_audit.py
+++ password_audit.py
@@ -1,11 +1,66 @@
-senha = input("Crie uma senha:")
-tamanho = len (senha)
-print(tamanho)
+senha = input("Digite uma senha: ")
 
-tem_numero = any(caractere.isdigit()for caractere in senha)
-print(tem_numero)
+tamanho = len(senha)
 
-if tamanho >= 8 and tem_numero:
-    print("A senha atende aos requisitos.")
+especiais = "!@#$%&*"
+
+tem_numero = any(caractere.isdigit() for caractere in senha)
+
+tem_maiuscula = any(caractere.isupper() for caractere in senha)
+
+tem_minuscula = any(caractere.islower() for caractere in senha)
+
+tem_especial = any(caractere in especiais for caractere in senha)
+
+senha_valida= True
+
+pontuacao = 0
+
+if tamanho < 8:
+    print("A senha precisa ter pelo menos 8 caracteres.")
+    senha_valida = false
+
 else:
-    print("A senha não atende aos requisitos.")
+        pontuacao += 1
+
+if not tem_numero:
+    print("A senha precisa de pelo menos um número")
+    senha_valida = false
+
+else:
+        pontuacao += 1
+
+if not tem_maiuscula:
+    print("A senha precisa de pelo menos uma letra maiúscula")
+    senha_valida = false
+
+else: 
+        pontuacao += 1
+
+if not tem_minuscula:
+    print("A senha precisa ter ao menos uma letra minúscula")
+    senha_valida = false
+
+else:
+        pontuacao += 1
+
+if not tem_especial:
+    print("A senha precisa ter ao menos um especial")
+    senha_valida = false
+
+else: 
+    pontuacao += 1
+
+
+if senha_valida:
+    print("Senha aprovada!")
+
+print("pontuação", pontuacao)
+
+if pontuacao == 5:
+    print("Força da senha: FORTE")
+elif pontuacao == 3:
+    print("Força da senha: MÉDIA")
+else:
+    print("Força da senha: FRACA")
+

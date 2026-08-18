257fd1fb4536394dea3b3de24d69c46ee4e51a55
Vitória Chuarts <vitoriachuarts@icloud.com>
2026-08-18 14:00:04 +0000

    analisador básico

--- password_audit.py
+++ password_audit.py
@@ -1,1 +1,11 @@
+senha = input("Crie uma senha:")
+tamanho = len (senha)
+print(tamanho)
 
+tem_numero = any(caractere.isdigit()for caractere in senha)
+print(tem_numero)
+
+if tamanho >= 8 and tem_numero:
+    print("A senha atende aos requisitos.")
+else:
+    print("A senha não atende aos requisitos.")
